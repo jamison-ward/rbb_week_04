e2e3d6df16518bea961b619c33977eb31d4acd4a
Jamison-Ward <oceanic.magma682@simplelogin.fr>
2025-10-08 12:40:04 -0400

    Added README

---
+++ 04_github_repo_config.R
@@ -0,0 +1,9 @@
+# Title: script_title
+# Author: Jamison Ward
+# Created: Wed, Oct 08 2025, 12:36:06
+
+library(usethis)
+
+use_github()
+
+use_readme_md()
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# rbb_week_04
+
+RStudio project from week 4 of The GRPAH Courses R Basics & Beyond
