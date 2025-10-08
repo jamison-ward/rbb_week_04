7e5a6be6024b2b1fd762f40dd9c1a7f2db95d477
Jamison-Ward <oceanic.magma682@simplelogin.fr>
2025-10-08 12:41:52 -0400

    Added script comments

--- 04_github_repo_config.R
+++ 04_github_repo_config.R
@@ -2,8 +2,8 @@
 # Author: Jamison Ward
 # Created: Wed, Oct 08 2025, 12:36:06
 
-library(usethis)
+library(usethis)  # Import usethis library to create repo on github
 
-use_github()
+use_github()  # Create a github repo
 
-use_readme_md()
+use_readme_md()  # Create a README.md file for the github repo
